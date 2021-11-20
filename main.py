ff6f21de70267b271320410bbc8a2b7c0fc17116
adam55 <adam.sitbon@gmail.com>
2021-11-20 11:38:28 +0100

    refactor

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     ETHERSCAN_TOKEN = os.getenv("ETHERSCAN_TOKEN")
     ETH_URL = os.getenv("ETH_URL")
     contract_address = "0x39b2d948b13fa10054e00b9d5f5eea75d272fb32"
-    x = Web3ContractListener(
+    contract_listener = Web3ContractListener(
         eth_url=ETH_URL,
         contract_address=contract_address,
         etherscan_token=ETHERSCAN_TOKEN,
     )
-    x.listen()
+    contract_listener.listen()
