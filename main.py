52755f2deb5e56292eb7b1d14613895b900f6468
adam55 <adam.sitbon@gmail.com>
2021-11-20 11:39:43 +0100

    fixed typo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from web3_listener.contract_listener import Web3ContractListener
+import os
+
+
 if __name__ == "__main__":
-    import os
     ETHERSCAN_TOKEN = os.getenv("ETHERSCAN_TOKEN")
     ETH_URL = os.getenv("ETH_URL")
     contract_address = "0x39b2d948b13fa10054e00b9d5f5eea75d272fb32"
